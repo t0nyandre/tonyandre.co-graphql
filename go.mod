ba2cda4896b75779241a4c9be77e43ecbb0141cc
t0nyandre <post@tonyandre.co>
2020-03-18 21:50:08 +0100

    Add user model and argon2id hashing for password

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/t0nyandre/go-graphql
 go 1.14
 
 require (
+	github.com/alexedwards/argon2id v0.0.0-20190612080829-01a59b2b8802 // indirect
 	github.com/go-chi/chi v4.0.3+incompatible // indirect
 	github.com/go-chi/cors v1.0.1 // indirect
 	github.com/google/uuid v1.1.1 // indirect
@@ -12,4 +13,6 @@ require (
 	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/lithammer/shortuuid v3.0.0+incompatible
 	github.com/mattdamon108/gqlmerge v0.2.3 // indirect
+	golang.org/x/crypto v0.0.0-20200317142112-1b76d66859c6 // indirect
+	golang.org/x/sys v0.0.0-20200317113312-5766fd39f98d // indirect
 )
---
+++ internal/model/user.go
@@ -0,0 +1,59 @@
+package model
+
+import (
+	"time"
+
+	"github.com/alexedwards/argon2id"
+	"github.com/jinzhu/gorm"
+)
+
+type User struct {
+	ID             string  `json:"_id,omitempty"`
+	FirstName      string  `json:"first_name,omitempty" db:"first_name"`
+	LastName       *string `json:"last_name,omitempty" db:"last_name"`
+	Email          string  `json:"email,omitempty"`
+	HashedPassword string  `json:"password,omitempty" db:"hashed_password"`
+	Confirmed      bool    `json:"confirmed,omitempty"`
+	CreatedAt      string  `json:"created_at,omitempty" db:"created_at"`
+	UpdatedAt      *string `json:"updated_at,omitempty" db:"updated_at"`
+}
+
+func (user *User) HashPassword() string {
+	customParams := argon2id.Params{
+		Iterations:  3,
+		Memory:      4096,
+		Parallelism: 1,
+		SaltLength:  16,
+		KeyLength:   32,
+	}
+
+	hash, err := argon2id.CreateHash(user.HashedPassword, &customParams)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return hash
+}
+
+func (user *User) VerifyPassword(password string) bool {
+	match, err := argon2id.ComparePasswordAndHash(password, user.HashedPassword)
+	if err != nil {
+		return false
+	}
+
+	return match
+}
+
+func (user *User) BeforeCreate(scope *gorm.Scope) error {
+	t := time.Now()
+	scope.SetColumn("HashedPassword", user.HashPassword())
+	scope.SetColumn("CreatedAt", t.Format(time.RFC822Z))
+	scope.SetColumn("Confirmed", false)
+	return nil
+}
+
+func (user *User) BeforeUpdate(scope *gorm.Scope) error {
+	t := time.Now()
+	scope.SetColumn("UpdatedAt", t.Format(time.RFC822Z))
+	return nil
+}
